6c2914187e58a226ab572b05c61c7c413baedcc3
JangSihoon <135976413+JangSihoon@users.noreply.github.com>
2024-02-07 21:26:46 +0900

    Title: 19단 출력, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/19단 출력/nineteen-times-table.py
+++ 240207/19단 출력/nineteen-times-table.py
@@ -1,6 +1,19 @@
+def even(i,j):
+    print(f"{i} * {j} = {i*j}",end = "\n")
+def odd(i,j):
+    if j==19:
+        print(f"{i} * {j} = {i*j}",end = " ")
+    else:
+        print(f"{i} * {j} = {i*j}",end = " / ")
+
 for i in range(1, 20):
     for j in range(1, 20):
-        print(f"{i} * {j} = {i * j}", end=" ")
-        if j % 2 == 0:
-            print(f"/ {i} * {j + 1} = {i * (j + 1)}", end=" ")
+        if j%2==0:
+            even(i,j)
+        else:
+            odd(i,j)
     print()
+
+# 1,1   1,2     1,3     1,4     1,5     
+# 1,6   1,7     1,8     1,9     1,10
+# 1,19
