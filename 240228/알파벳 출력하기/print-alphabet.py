0d56e47ceac240ced28e43cf41c306a1aa1be757
JangSihoon <135976413+JangSihoon@users.noreply.github.com>
2024-02-28 20:23:25 +0900

    Title: 알파벳 출력하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/알파벳 출력하기/print-alphabet.py
+++ 240228/알파벳 출력하기/print-alphabet.py
@@ -1,12 +1,12 @@
 # ?? -> 이게뭐지? -> 나 뭘해야되지? -> 대답 -> 나 뭘한거지? -> 대답
 n = int(input())
-cnt = 64
+alpha_idx = ord("A")-1
 for i in range(1,n+1):
     for j in range(i):
-        cnt += 1
-        if chr(cnt)==83:
-            cnt==64
-        print(chr(cnt),end = "")
+        alpha_idx += 1
+        if alpha_idx > ord("Z"):
+            alpha_idx = ord("A")
+        print(chr(alpha_idx),end = "")
     print()
 
 
