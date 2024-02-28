849078e7a420df0c424ce8f756744ea94933ab86
JangSihoon <135976413+JangSihoon@users.noreply.github.com>
2024-02-28 19:47:51 +0900

    Title: 알파벳 출력하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240228/알파벳 출력하기/print-alphabet.py
@@ -0,0 +1,13 @@
+# ?? -> 이게뭐지? -> 나 뭘해야되지? -> 대답 -> 나 뭘한거지? -> 대답
+n = int(input())
+cnt = 64
+for i in range(1,n+1):
+    for j in range(i):
+        cnt += 1
+        print(chr(cnt),end = "")
+    print()
+
+
+# 이건 abc 순서대로 나열하여 삼각형을 만드는 것이다.
+# 난 삼각형을 만들고 abc순서대로 채워넣어야한다.
+# 난 삼각형을 만들고 abc순서대로 채워넣었다.
